1b979196ba6bebe34619acaa7a1a8372c7f37c69
Eloy Pedrosa Garcia <pedrosaeloy9@gmail.com>
2022-12-12 01:28:46 +0100

    12/12/2022

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ app.get('/', function(req, res){
     res.sendFile(`${__dirname}/clients/index.html`)
 })
 
-server.listen(3000, function(){
+app.get('/rooms', function(req, res){
+    res.sendFile(`${__dirname}/clients/privateRoom.html`)
+})
+
+app.get('/contactame', function(req, res){
+    res.sendFile(`${__dirname}/clients/contact.html`)
+})
+
+
+server.listen(3000, () => {
     console.log(`Listening to port 3000`);
 })
+
